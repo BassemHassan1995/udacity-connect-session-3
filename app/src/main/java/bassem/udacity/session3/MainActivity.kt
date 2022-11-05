9275fc34936c7ec253975bb35a84171ae02f999f
Bassem Hassan <bassem.hassan1995@gmail.com>
2022-11-05 07:10:47 +0200

    Add Session 3

---
+++ app/src/main/java/bassem/udacity/session3/MainActivity.kt
@@ -0,0 +1,11 @@
+package bassem.udacity.session3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
